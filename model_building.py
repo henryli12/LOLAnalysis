79b59312672929b45341fcb1c35faaef1768ef7b
henry <xiaofengli2016@gmail.com>
2021-01-06 21:25:21 -0500

    modified model and added images/req.txt

--- model_building.py
+++ model_building.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     filename = 'data/cleaned_players_data.csv'
     df = pd.read_csv(filepath_or_buffer=filename, index_col=0)
     
-    df_model = df[['win', 'visionScore', 'turretKills',
-                'inhibitorKills', 'wardsPlaced', 'wardsKilled',
-                'kda', 'totalDamageDealt_per_min', 'totalDamageDealtToChampions_per_min',
-                'damageSelfMitigated_per_min', 'damageDealtToObjectives_per_min',
-                'totalDamageTaken_per_min', 'goldEarned_per_min',
-                'goldSpent_per_min', 'totalMinionsKilled_per_min',
-                'neutralMinionsKilled_per_min']]
+    df_model = df[['win', 'largestKillingSpree', 'visionScore', 'turretKills', 'inhibitorKills',
+                   'kda', 'totalDamageDealt_per_min', 'totalDamageDealtToChampions_per_min',
+                   'totalHeal_per_min', 'damageDealtToObjectives_per_min', 'totalDamageTaken_per_min',
+                   'goldEarned_per_min', 'goldSpent_per_min', 'totalMinionsKilled_per_min',
+                   'firstBloodKill', 'firstBloodAssist', 'firstTowerKill', 'firstTowerAssist',
+                   'firstInhibitorKill', 'firstInhibitorAssist']]
+    
     df_dum = pd.get_dummies(df_model)
     
     from sklearn.model_selection import train_test_split
@@ -32,21 +32,33 @@ if __name__ == "__main__":
     from sklearn.linear_model import LinearRegression, LogisticRegression, Lasso
     from sklearn.model_selection import cross_val_score
     
+    # Linear Model
     linreg = LinearRegression()
     linreg.fit(x_train, y_train)
     print('linear regression--------------------------------------')
-    print(linreg.predict(x_test[:5]))
+    print("Winrate prediction of x_test[:10]:")
+    print(np.round(linreg.predict(x_test[:10]), 2))
+    print("Cross Validation Accuracy: ")
     print(np.mean(cross_val_score(linreg, x_train, y_train, cv= 3)))
+    print()
     
+    # Logistic Model
     logreg = LogisticRegression(random_state=0, max_iter=10000)
     logreg.fit(x_train, y_train)
     print("logistic regression------------------------------------")
-    print(logreg.predict_proba(x_test[:5])[:,1])
+    print("Winrate prediction of x_test[:10]:")
+    print(np.round(logreg.predict_proba(x_test[:10])[:,1], 2))
+    print("Cross Validation Accuracy: ")
     print(np.mean(cross_val_score(logreg, x_train, y_train, cv= 3)))
+    print()
 
+    # Random Forest
     from sklearn.ensemble import RandomForestRegressor
     rf = RandomForestRegressor(random_state=0)
     rf.fit(x_train, y_train)
     print('random forest------------------------------------------')
-    print(rf.predict(x_test[:5]))
+    print("Winrate prediction of x_test[:10]:")
+    print(rf.predict(x_test[:10]))
+    print("Cross Validation Accuracy: ")
     print(np.mean(cross_val_score(rf,x_train,y_train, cv= 3)))
+    print()
